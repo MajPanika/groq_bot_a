be51a0bf8cc6a05c9623be40cbfa20bb3e3c6bfd
MajPanika <138772725+MajPanika@users.noreply.github.com>
2026-01-02 20:35:13 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+DEFAULT_MODEL = "llama-3.1-8b-instant"
